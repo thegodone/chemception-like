780f7a6ecf54b0c648e68ec8d3a5c753ebb30918
guillaume godin <guillaume.godin@gmail.com>
2018-08-17 13:30:33 +0200

    fix data path

--- rdkitpandareadsdf.py
+++ rdkitpandareadsdf.py
@@ -2,6 +2,6 @@ import rdkit
 from rdkit import Chem
 from rdkit.Chem import PandasTools
 
-frame = PandasTools.LoadSDF('dhfr_3d.sd',smilesName='smiles', molColName='Molecule', includeFingerprints=False)
+frame = PandasTools.LoadSDF('data/dhfr_3d.sd',smilesName='smiles', molColName='Molecule', includeFingerprints=False)
 
-frame.to_hdf('Sutherland_DHFR.h5', key='data', mode='w')
+frame.to_hdf('data/Sutherland_DHFR.h5', key='data', mode='w')
